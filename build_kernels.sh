477993846ce85cc227c7b32930d3bf132040d175
Jimbo77 <chupyalcohol@gmail.com>
2020-10-16 15:57:33 -0600

    added script to automate builds for all variants

---
+++ build_kernels.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+cd ~/build/TestSam/sm8250
+
+./build_kernel_JimboK.sh z3q 1.4.0
+
+sleep 10
+
+./build_kernel_JimboK.sh x1q 1.4.0
+
+sleep 10
+
+./build_kernel_JimboK.sh y2q 1.4.0
+
+sleep 10
+
+./build_kernel_JimboK.sh c1q 1.4.0
+
+sleep 10
+
+./build_kernel_JimboK.sh c2q 1.4.0
+
+rm -rf out
+
+make mrproper
