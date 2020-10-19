cfa8ae95f944c45a0b0e0b5c6cc8fd1f5c4eb1f8
Jimbo77 <chupyalcohol@gmail.com>
2020-10-19 13:07:57 -0600

    Defined VERSION in build script as a parameter to make it easier to compile all supported builds

--- build_kernels.sh
+++ build_kernels.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -x
 
-./build_kernel_JimboK.sh z3q 1.4.0
+VERSION=$1
+
+./build_kernel_JimboK.sh z3q $1
 
 sleep 10
 
-./build_kernel_JimboK.sh x1q 1.4.0
+./build_kernel_JimboK.sh x1q $1
 
 sleep 10
 
-./build_kernel_JimboK.sh y2q 1.4.0
+./build_kernel_JimboK.sh y2q $1
 
 sleep 10
 
-./build_kernel_JimboK.sh c1q 1.4.0
+./build_kernel_JimboK.sh c1q $1
 
 sleep 10
 
-./build_kernel_JimboK.sh c2q 1.4.0
+./build_kernel_JimboK.sh c2q $1
 
 rm -rf out
 
